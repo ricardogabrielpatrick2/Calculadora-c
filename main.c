894d603cfe84e78c3312938e8726e0120531f780
Gabriel Patrick <seu_email_do_github>
2026-06-17 15:34:04 -0300

    Versao final da calculadora

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>  // Biblioteca matematica.
 
 float adicao(float a, float b){
 return a + b;
@@ -7,16 +8,24 @@ float subtracao(float a, float b){
 return a - b;
 }
 float multiplicacao(float a, float b){
-    return a * b;
+    return a * b;                                 // Quando declarado antes do main, o compilador ja a conhece.
 }
 float divisao(float a, float b){
     return a / b;
+}
+float porcentagem(float percentual, float valor){
+    return (percentual * valor) / 100;
+}
+float media(float a, float b){
+    return (a + b) /2;
 }
 
 int main() {
     float a, b;
     int opcao;
-do{
+
+do{         // Estrutura de repetição, repete tudo ate o usuario digitar 0.
+
         printf("=============\n");
          printf(" CALCULADORA \n");
         printf("=============\n");
@@ -25,7 +34,11 @@ do{
         printf("2 - Subtracao\n");
         printf("3 - Multiplicacao\n");
         printf("4 - Divisao\n");
-        printf("0. Sair\n");
+        printf("5 - potegcc main.cncia\n");
+        printf("6 - Raiz quadrada\n");
+        printf("7 - Porcentagem\n");
+        printf("8 - Media\n");
+        printf("0 - Sair\n");
 
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
@@ -36,11 +49,19 @@ do{
         break;
     }
 
-    printf("Digite o primeiro numero: ");
-    scanf("%f", &a);
+     if(opcao == 6){
+        printf("Digite o numero: ");
+        scanf("%f", &a);
+        
+    }else{
 
-    printf("Digite o segundo numero: ");
-    scanf("%f", &b);
+        printf("Digite o primeiro numero: ");
+        scanf("%f", &a);
+
+        printf("Digite o segundo numero: ");
+        scanf("%f", &b);
+}
+
 
     switch(opcao) {
         case 1:
@@ -48,18 +69,37 @@ do{
              break;
 
         case 2:
-             printf("Resultado: %.2f\n\n", a - b);
+             printf("Resultado: %.2f\n\n", subtracao(a, b));
              break;
 
         case 3:
-            printf("Resultado: %.2f\n\n", a * b);
+            printf("Resultado: %.2f\n\n", multiplicacao(a, b));
             break;
 
         case 4:
             if(b != 0)
-                printf("Resultado: %.2f\n\n", a / b);
+                printf("Resultado: %.2f\n\n", divisao(a, b));
             else
                 printf("Erro: divisao por zero!\n\n");
+            break;
+
+        case 5:
+                printf("resultado: %.2f\n\n", pow(a,b));
+            break;
+            
+        case 6:
+            if(a >= 0)
+                printf("Resultado: %.2f\n\n", sqrt(a));
+            else
+                printf("Erro: Numero negativo!\n\n");
+            break;
+
+        case 7:
+                printf("Resultado: %.2f\n\n", porcentagem(a, b));
+            break;
+
+            case 8: 
+                printf("Resultado: %.2f\n\n", media(a, b));
             break;
 
         default:
